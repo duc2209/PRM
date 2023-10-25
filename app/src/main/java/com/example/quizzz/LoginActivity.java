86e265f04b7403774f57ce4b1e39ab49fd80c1dd
NguyenVanVuong <hayasilver123@gmail.com>
2023-10-25 22:22:25 +0700

    Vuong undo comment

--- app/src/main/java/com/example/quizzz/LoginActivity.java
+++ app/src/main/java/com/example/quizzz/LoginActivity.java
@@ -10,42 +10,42 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-//    private EditText email,pass;
-//    private Button loginB;
-//    private TextView forgotPassB,singupB;
+    private EditText email,pass;
+    private Button loginB;
+    private TextView forgotPassB,singupB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        email = findViewById(R.id.email);
-//        pass = findViewById(R.id.password);
-//        loginB = findViewById(R.id.loginB);
-//        forgotPassB = findViewById(R.id.forgot_pass);
-//        singupB = findViewById(R.id.singupB);
-//
-//        loginB.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(validateData()){
-//                    login();
-//                }
-//            }
-//        });
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        loginB = findViewById(R.id.loginB);
+        forgotPassB = findViewById(R.id.forgot_pass);
+        singupB = findViewById(R.id.singupB);
+
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validateData()){
+                    login();
+                }
+            }
+        });
     }
 
-//    private boolean validateData(){
-//      if(email.getText().toString().isEmpty()){
-//          email.setError("Enter E-Mail Id");
-//          return false;
-//      }
-//      if(pass.getText().toString().isEmpty()){
-//          pass.setError("Enter Password");
-//          return false;
-//      }
-//      return true;
-//    }
-//
-//    private void login(){
-//
-//    }
+    private boolean validateData(){
+      if(email.getText().toString().isEmpty()){
+          email.setError("Enter E-Mail Id");
+          return false;
+      }
+      if(pass.getText().toString().isEmpty()){
+          pass.setError("Enter Password");
+          return false;
+      }
+      return true;
+    }
+
+    private void login(){
+
+    }
 }
